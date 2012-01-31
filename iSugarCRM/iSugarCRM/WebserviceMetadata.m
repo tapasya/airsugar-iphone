2a4cd7fc13cce482c297330cfc89c66e76776ca1
satyavrat <satyavrat_mudgil@yahoo.com>
2012-01-31 20:58:49 +0530

    Remove code duplication in WebserviceMetadata
    Add method formatRequest in WebserviceMetadata

--- iSugarCRM/iSugarCRM/WebserviceMetadata.m
+++ iSugarCRM/iSugarCRM/WebserviceMetadata.m
@@ -21,6 +21,9 @@ static inline NSString* httpMethodAsString(HTTPMethod method){
             break;
     }
 };
+@interface WebserviceMetadata ()
+-(NSURLRequest*)formatRequest;
+@end
 
 @implementation WebserviceMetadata
 @synthesize urlParameters,postParameters,headers,endpoint,method,moduleName;
@@ -60,62 +63,12 @@ static inline NSString* httpMethodAsString(HTTPMethod method){
 -(NSURLRequest*)getRequest
 {
     //append url parameters
-    
-    NSMutableString *urlWithParams = [[NSMutableString alloc] init];
-    [urlWithParams appendString:endpoint];
-    int index = 0;
     NSMutableDictionary *restDataDictionary = [[OrderedDictionary alloc] init];
     [restDataDictionary setObject:session forKey:@"session"];
     [restDataDictionary  setObject:moduleName forKey:@"module_name"];
     NSString *restDataString = [restDataDictionary JSONString];
     [self setUrlParam:restDataString forKey:@"rest_data"];
-    for(NSDictionary *urlParam in urlParameters)
-    {
-        NSString *key = [[urlParam allKeys] objectAtIndex:0];
-        if(index++ == 0)
-        {
-            [urlWithParams appendString:[NSString stringWithFormat:@"?%@=%@",key,[urlParam valueForKey:key]]];
-        }
-        else
-        {
-            [urlWithParams appendString:[NSString stringWithFormat:@"&%@=%@",key,[urlParam valueForKey:key]]];
-        }
-    }
-    NSLog(@"url string: %@",urlWithParams);
-    //set url
-    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:[urlWithParams stringByAddingPercentEscapesUsingEncoding:NSASCIIStringEncoding]]];
-    
-    //set http method
-    [request setHTTPMethod:httpMethodAsString(method)];
-    
-    //set http body if request is a post
-    if(method == HTTPMethodPOST){
-        NSMutableString *postData = [[NSMutableString alloc] init];
-        index = 0;
-        for(NSString *key in [postParameters allKeys]){  
-            if(index++ == 0)
-            {
-                [postData appendString:[NSString stringWithFormat:@"%@=%@",key,[postParameters valueForKey:key]]];
-            }
-            else
-            {
-                [postData appendString:[NSString stringWithFormat:@"&%@=%@",key,[postParameters valueForKey:key]]];
-            }
-        }
-        
-        if (postData.length > 0) {
-            [request setHTTPBody:[postData dataUsingEncoding:NSUTF8StringEncoding]];
-        }
-    }
-    
-    //set the headers
-    for(NSString *headerKey in [headers allKeys])
-    {
-        [request setValue:headerKey forHTTPHeaderField:[headers valueForKey:headerKey]];
-    }
-    
-    return request;
-    
+    return [self formatRequest];
 }
 
 
@@ -125,9 +78,6 @@ static inline NSString* httpMethodAsString(HTTPMethod method){
         return [self getRequest];
     }
     //append url parameters
-    NSMutableString *urlWithParams = [[NSMutableString alloc] init];
-    [urlWithParams appendString:endpoint];
-    int index = 0;
     NSMutableDictionary *restDataDictionary = [[OrderedDictionary alloc] init];
     [restDataDictionary setObject:session forKey:@"session"];
     [restDataDictionary  setObject:moduleName forKey:@"module_name"];
@@ -137,6 +87,14 @@ static inline NSString* httpMethodAsString(HTTPMethod method){
     // [restDataDictionary  setObject: forKey:@"select_fields"];
     NSString *restDataString = [restDataDictionary JSONString];
     [self setUrlParam:restDataString forKey:@"rest_data"];
+    return [self formatRequest];
+}
+
+-(NSURLRequest*)formatRequest
+{
+    NSMutableString *urlWithParams = [[NSMutableString alloc] init];
+    [urlWithParams appendString:endpoint];
+    int index = 0;
     for(NSDictionary *urlParam in urlParameters)
     {
         NSString *key = [[urlParam allKeys] objectAtIndex:0];
