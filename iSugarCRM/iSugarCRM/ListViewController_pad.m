1cd8cd8f4833034fc3e7fa7eaaa17fe64a3b0ec2
Tapasya K H S <tapasyakhs@gmail.com>
2012-04-17 15:16:33 +0530

    dismissing irrelavent popovers on orientation changes

--- iSugarCRM/iSugarCRM/ListViewController_pad.m
+++ iSugarCRM/iSugarCRM/ListViewController_pad.m
@@ -17,6 +17,7 @@
 {
     @private
     UIPopoverController *popOver;
+    UIActionSheet *_actionSheet;
 }
 @end
 @implementation ListViewController_pad
@@ -61,7 +62,16 @@
 {
     [super viewDidLoad];
     self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Modules" style:UIBarButtonItemStyleBordered target:self  action:@selector(showDashboard:)];
+    if(UIInterfaceOrientationIsPortrait([UIApplication sharedApplication].statusBarOrientation))
+    {
+        self.navigationItem.rightBarButtonItem.customView.hidden = YES;
+    }
     
+}
+
+-(void) viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
 }
 
 -(IBAction) showDashboard:(id)sender
@@ -79,6 +89,32 @@
 {
     // Return YES for supported orientations
 	return YES;
+}
+
+-(void) willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration
+{
+    if(popOver)
+    {
+        [popOver dismissPopoverAnimated:YES];
+        popOver = nil;
+    }
+}
+
+-(void) didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    if(self.editing)
+    {
+        [self toggleEditing];
+    }
+    if(_actionSheet && [_actionSheet isVisible])
+    {
+        [_actionSheet dismissWithClickedButtonIndex:2 animated:YES];
+        _actionSheet = nil;
+    }
+    if(self.navigationItem.rightBarButtonItem.customView)
+    {
+        self.navigationItem.rightBarButtonItem.customView.hidden = UIInterfaceOrientationIsPortrait([UIApplication sharedApplication].statusBarOrientation);
+    }
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
@@ -107,7 +143,7 @@
 
 -(void)showActionSheet{
     //[self.actionSheet showInView:self.view];
-    UIActionSheet *_actionSheet = [[UIActionSheet alloc] initWithTitle:nil delegate:nil cancelButtonTitle:nil destructiveButtonTitle:nil otherButtonTitles:nil];
+    _actionSheet = [[UIActionSheet alloc] initWithTitle:nil delegate:nil cancelButtonTitle:nil destructiveButtonTitle:nil otherButtonTitles:nil];
     [_actionSheet addButtonWithTitle:@"Add"];
     [_actionSheet addButtonWithTitle:@"Delete"];
     [_actionSheet addButtonWithTitle:@"Cancel"];
@@ -122,7 +158,8 @@
     msvc.moduleName = self.title;
     msvc.delegate = self;
     popOver = [[UIPopoverController alloc] initWithContentViewController:msvc];
-    [popOver presentPopoverFromBarButtonItem:self.navigationItem.rightBarButtonItem permittedArrowDirections:UIPopoverArrowDirectionUp animated:YES];
+    CGRect frame = self.navigationItem.rightBarButtonItem.customView.frame;
+    [popOver presentPopoverFromRect:CGRectMake(frame.origin.x, 1, frame.size.width/3, 1) inView:self.view permittedArrowDirections:UIPopoverArrowDirectionUp animated:YES];
 }
 
 -(void) sortSelectionChanged
